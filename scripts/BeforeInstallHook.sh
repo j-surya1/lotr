36c68037f78d79ea453afbcf4a5c931e33e1d6ce
Surya J <jaya.jsurya@sonata-software.com>
2023-06-12 15:52:24 +0530

    updated node

--- scripts/BeforeInstallHook.sh
+++ scripts/BeforeInstallHook.sh
@@ -2,6 +2,6 @@
 set -e
 yum update -y
 yum install -y gcc-c++ make
-curl -sL https://rpm.nodesource.com/setup_10.x | sudo bash -
+curl -sL https://rpm.nodesource.com/setup_14.x | sudo bash -
 yum install -y nodejs
 
