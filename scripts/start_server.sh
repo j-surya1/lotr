0b5773fe72d2ba1c01a85192a1c9169b9b6f53c8
Surya J <jaya.jsurya@sonata-software.com>
2023-06-12 15:59:29 +0530

    pm2

--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -2,5 +2,10 @@
 # Stop all servers and start the server
 set -e
 cd /usr/share/nginx/html
+
 pm2 stop all
-sudo -E pm2 start server.js
+sudo service pm2 stop
+sudo rm -rf /root/.pm2
+sudo rm -rf /home/username/.pm2
+sudo service pm2 start
+NODE_ENV=staging pm2 -x -i 1 start server.js
