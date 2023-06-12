f28d618e8cce13844b0c0460c7cadc562f7c0492
Surya J <jaya.jsurya@sonata-software.com>
2023-06-12 11:29:38 +0530

    changed nodejs installation method

--- scripts/BeforeInstallHook.sh
+++ scripts/BeforeInstallHook.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 set -e
 yum update -y
-curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.3/install.sh | bash
-. ~/.nvm/nvm.sh
-nvm install 10
+yum install -y gcc-c++ make
+curl -sL https://rpm.nodesource.com/setup_10.x | sudo bash -
+yum install -y nodejs
